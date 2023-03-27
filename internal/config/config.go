f162a59d019c87888eb8c9f1343596af5f426e2a
gambruh <salikh.small@gmail.com>
2023-03-27 14:34:47 +0400

    add dockerfile

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,11 +8,11 @@ import (
 )
 
 type Config struct {
-	Accrual   string `env:"ACCRUAL_SYSTEM_ADDRESS" envDefault:"127.0.0.1:5557"`
-	Address   string `env:"RUN_ADDRESS" envDefault:"127.0.0.1:8080"`
+	Accrual   string `env:"ACCRUAL_SYSTEM_ADDRESS" envDefault:"localhost:5557"`
+	Address   string `env:"RUN_ADDRESS" envDefault:"localhost:8080"`
 	RateLimit int    `env:"RATE_LIMIT" envDefault:"1"`
 	Key       string `env:"HASH_KEY" envDefault:""`
-	Database  string `env:"DATABASE_URI" envDefault:""`
+	Database  string `env:"DATABASE_URI" envDefault:"postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"`
 	Storage   bool
 }
 
@@ -33,9 +33,9 @@ var (
 )
 
 func InitFlags() {
-	Flags.Address = flag.String("a", "127.0.0.1:8080", "server address in format host:port")
+	Flags.Address = flag.String("a", "localhost:8080", "server address in format host:port")
 	Flags.Database = flag.String("d", "", "postgres database uri")
-	Flags.Accrual = flag.String("r", "127.0.0.1:5557", "accrual system address in format host:port")
+	Flags.Accrual = flag.String("r", "localhost:8081", "accrual system address in format host:port")
 	Flags.Key = flag.String("k", "abcd", "key to hash")
 	Flags.RateLimit = flag.Int("l", 1, "max amount of goroutines working simultaneously")
 	Flags.Storage = flag.Bool("s", false, "inmemory storage for lazy debugging")
